bb1c19e371dab0c77d41e757ee4bf8d2e7f4d5b6
Cristian <adrian.cristian.st@gmail.com>
2024-12-02 04:39:44 -0300

    style: realterado o layout da página de busca

--- src/controllers/SearchController.ts
+++ src/controllers/SearchController.ts
@@ -5,10 +5,14 @@ export class SearchController {
   async search(req: Request, res: Response) {
     try {
       let query = req.query.q;
-      
+
+      if (query === "" || query === undefined || query === null) {
+          res.render('search/render', { books: [] });
+      }
+
       const books = await getBooks(query);
 
-      res.render('search/render', { books: books });
+      res.render('search/render', { books: books, query: query });
 
       query = '';
     } catch (err) {
--- src/service/GoogleBooksService.ts
+++ src/service/GoogleBooksService.ts
@@ -5,15 +5,16 @@ const apiKey = 'AIzaSyC0M094uHsFpQwr-sIS1bAw0Lg9Kwnidgo';
 
 export async function getBooks(query) {
     console.log(query)
-    if (query === undefined || query === null || query === '') {
-        return [];
-    }
 
-    else {
+    // ${googleBooksApiUrl}?q=${query}&key=${apiKey}&langRestrict=pt
+    const url = `${googleBooksApiUrl}?q=${query}&key=${apiKey}`;
+    const response = await axios.get(url);
 
-        const url = `${googleBooksApiUrl}?q=${query}&key=${apiKey}`;
-        const response = await axios.get(url);
-        console.log(response.data.items)
-        return response.data.items.slice(0, 6);
-    }
+    console.log(response.data.items)
+
+    // removendo livros sem categorias (e, possivelmente, capas)
+    // .filter(item => item.volumeInfo.categories !== undefined)
+    return response.data.items
+    .slice(0, 8);
+
 }
